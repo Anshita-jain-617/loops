c7b5bbf5fa6167039ca47f4593c9d5e6deb7e950
Anshita-jain-617 <116895741+Anshita-jain-617@users.noreply.github.com>
2022-10-29 09:57:44 +0530

    Add files via upload

---
+++ 1-10  print.py
@@ -0,0 +1,27 @@
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+
+
+# i=1
+# n=int(input("enter : "))
+# while i<=n:
+#     print(i)
+#     i+=1
+
+
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+
+
+
+# i=int(input("enter : "))
+# while i>=1:
+#     print(i)
+#     i-=1
---
+++ sum .py
@@ -0,0 +1,9 @@
+# n=int(input("enter"))
+# i=0
+# sum=0
+# while i<=n:
+#     sum=sum+i
+#     i+=1
+# print("sum",sum)
+
+
